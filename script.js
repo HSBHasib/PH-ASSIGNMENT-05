9c2954ab701687f55d4432d837fb3bff10ea4475
Hasib <hasibhsb19@gmail.com>
2026-03-06 23:26:33 +0600

    add code js code for 'login page'

---
+++ script.js
@@ -0,0 +1,41 @@
+const fieldset = document.querySelector('fieldset');
+const userName = document.getElementById('name');
+const pass = document.getElementById('pass');
+const signInBtn = document.getElementById('signIn-btn');
+
+// Input Validation
+const inputValidation = () => {
+    let enteredName = userName.value;
+    let enteredPass = pass.value;
+
+    // Remove Previous input field text
+    userName.value = "";
+    pass.value = "";
+
+    // Default UserName and Password
+    const correctUser = "admin";
+    const correctPass = "admin123";
+
+    // Check is user enter correct userName and Password 
+    if (enteredName === "" || enteredPass === "") {
+        alert("Please fill in both Username and Password!");
+    } 
+    else if (enteredName !== correctUser) {
+        alert("Incorrect Username! Please check again.");
+    } 
+    else if (enteredPass !== correctPass) {
+        alert("Incorrect Password! Please check again.");
+    } 
+    else {
+        alert("Welcome Admin.");
+        window.location.href = "home.html"; 
+    }
+}
+
+signInBtn.addEventListener('click', inputValidation);
+
+window.addEventListener('keydown', (dets) => {
+    if (dets.key === 'Enter') {
+        inputValidation();
+    }
+});
